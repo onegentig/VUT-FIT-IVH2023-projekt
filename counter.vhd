7c3cb3a0946b9906fa825d0ed707cb03d7d41f00
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-03-14 03:04:12 +0100

    feat: finish the three-channel counter

--- counter.vhd
+++ counter.vhd
@@ -1,10 +1,10 @@
--- Citac s volitelnou frekvenci
--- IVH projekt - ukol 2
--- Autor: Onegen Something <xonege99@vutbr.cz>
+-- Citac s volitelnou frekvenciou
+-- @author Onegen Something <xonege99@vutbr.cz>
+-- @date 2023-03-14
+--
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
--- v pripade nutnosti muzete nacist dalsi knihovny
 
 ENTITY counter IS
 	GENERIC (
@@ -20,11 +20,58 @@ ENTITY counter IS
 	);
 END counter;
 
-ARCHITECTURE Behavioral OF counter IS
-
+ARCHITECTURE behavioral OF counter IS
+	SIGNAL CNT1 : INTEGER RANGE 0 TO OUT1_PERIOD - 1 := 0;
+	SIGNAL CNT2 : INTEGER RANGE 0 TO OUT2_PERIOD - 1 := 0;
+	SIGNAL CNT3 : INTEGER RANGE 0 TO OUT3_PERIOD - 1 := 0;
 BEGIN
-	-- Například čítač s periodou OUT1_PERIOD = 3 (t.j., každé 3 cykly)
-	-- aktivuje na právě na jeden hodinový cyklus signál EN1. Analogicky 
-	-- to bude fungovat i pro další dva kanály 
 
-END Behavioral;
+	-- Counter 1
+	p_cnt1 : PROCESS (CLK, RESET) BEGIN
+		IF RESET = '1' THEN
+			CNT1 <= 0;
+			EN1  <= '0';
+		ELSIF rising_edge(CLK) THEN
+			IF CNT1 = OUT1_PERIOD - 1 THEN
+				CNT1 <= 0;
+				EN1  <= '1';
+			ELSE
+				CNT1 <= CNT1 + 1;
+				EN1  <= '0';
+			END IF;
+		END IF;
+	END PROCESS;
+
+	-- Counter 2
+	p_cnt2 : PROCESS (CLK, RESET) BEGIN
+		IF RESET = '1' THEN
+			CNT2 <= 0;
+			EN2  <= '0';
+		ELSIF rising_edge(CLK) THEN
+			IF CNT2 = OUT2_PERIOD - 1 THEN
+				CNT2 <= 0;
+				EN2  <= '1';
+			ELSE
+				CNT2 <= CNT2 + 1;
+				EN2  <= '0';
+			END IF;
+		END IF;
+	END PROCESS;
+
+	-- Counter 3
+	p_cnt3 : PROCESS (CLK, RESET) BEGIN
+		IF RESET = '1' THEN
+			CNT3 <= 0;
+			EN3  <= '0';
+		ELSIF rising_edge(CLK) THEN
+			IF CNT3 = OUT3_PERIOD - 1 THEN
+				CNT3 <= 0;
+				EN3  <= '1';
+			ELSE
+				CNT3 <= CNT3 + 1;
+				EN3  <= '0';
+			END IF;
+		END IF;
+	END PROCESS;
+
+END behavioral;
