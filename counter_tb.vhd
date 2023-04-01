63d813e2dde61e43835fe6d13c017f232641805d
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-04-02 01:52:36 +0200

    test: add reset tests
    Added testing of 'RESET' signal (tests all EN for 10 CLK). Shortened main test loop to check 50 CLK (making it 60 CLK overall).

--- counter_tb.vhd
+++ counter_tb.vhd
@@ -39,12 +39,12 @@ BEGIN
 	-- Instaciacia jednotky pod testom (UUT)
 	--    Pocitejte s tim, ze pri zkouseni pobezi testbench 100 ms
 	uut : ENTITY work.counter
-		GENERIC MAP(
+		GENERIC MAP (
 			OUT1_PERIOD => 2,
 			OUT2_PERIOD => 3,
 			OUT3_PERIOD => 5
 		)
-		PORT MAP(
+		PORT MAP (
 			CLK   => CLK,
 			RESET => RESET,
 			EN1   => EN1,
@@ -67,6 +67,7 @@ BEGIN
 	-- Stimulus
 	stim_proc : PROCESS IS
 		VARIABLE clk_i      : INTEGER := 1;
+		VARIABLE passed_rst : BOOLEAN := FALSE;
 		VARIABLE passed_now : BOOLEAN := FALSE;
 		VARIABLE cnt_pass   : NATURAL := 0;
 		VARIABLE cnt_fail   : NATURAL := 0;
@@ -80,23 +81,23 @@ BEGIN
 		REPORT "======================================" SEVERITY NOTE;
 		REPORT " " SEVERITY NOTE;
 
-		WHILE clk_i <= 65 LOOP
+		WHILE clk_i <= 50 LOOP
 			WAIT FOR CLK_period;
 
 			-- Test citacov
 			ASSERT EN1 = b2sl(clk_i MOD 2 = 0) REPORT "FAIL! Nespravny EN1 v CLK "
-				& INTEGER'image(clk_i) SEVERITY ERROR;
+			& INTEGER'image(clk_i) SEVERITY ERROR;
 			ASSERT EN2 = b2sl(clk_i MOD 3 = 0) REPORT "FAIL! Nespravny EN2 v CLK "
-				& INTEGER'image(clk_i) SEVERITY ERROR;
+			& INTEGER'image(clk_i) SEVERITY ERROR;
 			ASSERT EN3 = b2sl(clk_i MOD 5 = 0) REPORT "FAIL! Nespravny EN3 v CLK "
-				& INTEGER'image(clk_i) SEVERITY ERROR;
+			& INTEGER'image(clk_i) SEVERITY ERROR;
 
 			-- Celkovy stav
 			passed_now := (EN1 = b2sl(clk_i MOD 2 = 0))
 				AND (EN2 = b2sl(clk_i MOD 3 = 0))
 				AND (EN3 = b2sl(clk_i MOD 5 = 0));
 			ASSERT passed_now = FALSE REPORT "PASS! Vsetky citace spravne v CLK "
-				& INTEGER'image(clk_i) SEVERITY NOTE;
+			& INTEGER'image(clk_i) SEVERITY NOTE;
 
 			IF passed_now = TRUE THEN
 				cnt_pass := cnt_pass + 1;
@@ -107,11 +108,35 @@ BEGIN
 			clk_i := clk_i + 1;
 		END LOOP;
 
+		-- Testy resetu
+		passed_rst := TRUE;
+		RESET       <= '1';
+
+		WHILE clk_i <= 60 LOOP
+			WAIT FOR CLK_period;
+
+			passed_now := (EN1 = '0')
+				AND (EN2 = '0')
+				AND (EN3 = '0');
+			ASSERT passed_now = TRUE REPORT "FAIL! Nefunkcny reset v CLK "
+				& INTEGER'image(clk_i) SEVERITY ERROR;
+
+			IF passed_now = TRUE THEN
+				cnt_pass := cnt_pass + 1;
+			ELSE
+				cnt_fail   := cnt_fail + 1;
+				passed_rst := FALSE;
+			END IF;
+
+			clk_i := clk_i + 1;
+		END LOOP;
+		ASSERT passed_rst = FALSE REPORT "PASS! Reset funguje v CLK 51-60" SEVERITY NOTE;
+
 		-- Vypis vysledkov
 		REPORT "======================================" SEVERITY NOTE;
 		REPORT " " SEVERITY NOTE;
 		REPORT "Citace spravne " & NATURAL'image(cnt_pass) & "-krat a chybne "
-			& NATURAL'image(cnt_fail) & "-krat za 65 CLK." SEVERITY NOTE;
+			& NATURAL'image(cnt_fail) & "-krat za 60 CLK." SEVERITY NOTE;
 		DONE <= TRUE;
 		WAIT;
 	END PROCESS;
