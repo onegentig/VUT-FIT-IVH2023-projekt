9a82cc4f9ec28933af272996bd50fbfb0ce23770
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-05-03 00:50:52 +0200

    fix: fix `DIR_TOP` scrolling down

--- column.vhd
+++ column.vhd
@@ -3,53 +3,53 @@
 -- @date 2023-04-22
 --
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-LIBRARY work;
-USE work.effects_pack.ALL;
+library work;
+use work.effects_pack.all;
 
-ENTITY column IS
-	GENERIC (
-		N : POSITIVE := 8
+entity column is
+	generic (
+		N : positive := 8
 	);
 
-	PORT (
-		CLK         : IN STD_LOGIC;
-		RESET       : IN STD_LOGIC;
-		STATE       : BUFFER STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-		INIT_STATE  : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-		NEIGH_LEFT  : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-		NEIGH_RIGHT : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-		DIRECTION   : IN DIRECTION_T;
-		EN          : IN STD_LOGIC
+	port (
+		CLK         : in     std_logic;
+		RESET       : in     std_logic;
+		STATE       : buffer std_logic_vector(N - 1 downto 0);
+		INIT_STATE  : in     std_logic_vector(N - 1 downto 0);
+		NEIGH_LEFT  : in     std_logic_vector(N - 1 downto 0);
+		NEIGH_RIGHT : in     std_logic_vector(N - 1 downto 0);
+		DIRECTION   : in     DIRECTION_T;
+		EN          : in     std_logic
 	);
-END column;
+end column;
 
-ARCHITECTURE behavioral OF column IS
-
-BEGIN
-	PROCESS (CLK, RESET) BEGIN
-		IF RESET = '1' THEN
+architecture behavioral of column is
+begin
+	process (CLK, RESET)
+	begin
+		if RESET = '1' then
 			----- Reset -----
 			STATE <= INIT_STATE;
-		ELSIF rising_edge(CLK) AND (EN = '1') THEN
-			IF (DIRECTION = DIR_LEFT) THEN
+		elsif rising_edge(CLK) and (EN = '1') then
+			if (DIRECTION = DIR_LEFT) then
 				----- Posun vlavo -----
 				STATE <= NEIGH_RIGHT;
-			ELSIF (DIRECTION = DIR_RIGHT) THEN
+			elsif (DIRECTION = DIR_RIGHT) then
 				----- Posun vpravo -----
 				STATE <= NEIGH_LEFT;
-			ELSIF (DIRECTION = DIR_TOP) THEN
+			elsif (DIRECTION = DIR_TOP) then
 				----- Shift nahor -----
-				STATE(N - 1 DOWNTO 1) <= STATE(N - 2 DOWNTO 0);
-				STATE(0)              <= '0';
-			ELSIF (DIRECTION = DIR_TOP_BOTTOM) THEN
+				STATE(N - 2 downto 0) <= STATE(N - 1 downto 1);
+				STATE(N - 1)          <= '0';
+			elsif (DIRECTION = DIR_TOP_BOTTOM) then
 				----- Ripple efekt -----
-				STATE(N - 1 DOWNTO (N / 2) + 1) <= STATE(N - 2 DOWNTO (N / 2));
-				STATE((N / 2) - 1 DOWNTO 0)     <= STATE((N / 2) DOWNTO 1);
+				STATE(N - 1 downto (N / 2) + 1) <= STATE(N - 2 downto (N / 2));
+				STATE((N / 2) - 1 downto 0)     <= STATE((N / 2) downto 1);
 				STATE((N / 2))                  <= '0';
-			END IF;
-		END IF;
-	END PROCESS;
-END behavioral;
+			end if;
+		end if;
+	end process;
+end behavioral;
--- column_tb.vhd
+++ column_tb.vhd
@@ -3,46 +3,46 @@
 -- @date 2023-04-30
 -- 
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-LIBRARY work;
-USE work.effects_pack.ALL;
+library work;
+use work.effects_pack.all;
 
-ENTITY column_tb IS
-END column_tb;
+entity column_tb is
+end column_tb;
 
-ARCHITECTURE behavior OF column_tb IS
+architecture behavior of column_tb is
 	-- Vstupy
-	SIGNAL CLK          : STD_LOGIC   := '0';
-	SIGNAL EN           : STD_LOGIC   := '0';
-	SIGNAL RESET        : STD_LOGIC   := '0';
-	SIGNAL DIRECTION    : DIRECTION_T := DIR_LEFT;
+	signal CLK       : std_logic   := '0';
+	signal EN        : std_logic   := '0';
+	signal RESET     : std_logic   := '0';
+	signal DIRECTION : DIRECTION_T := DIR_LEFT;
 
 	-- Hodinova perioda
-	CONSTANT CLK_period : TIME        := 10 ns;
+	constant CLK_period : time := 10 ns;
 
 	-- Pamat ROM
-	TYPE MATRIX_T IS ARRAY (0 TO 2) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-	CONSTANT ROM : MATRIX_T := (
+	type MATRIX_T is array (0 to 2) of std_logic_vector(7 downto 0);
+	constant ROM : MATRIX_T := (
 		X"AA",
 		X"CC",
 		X"FF"
 	);
 
 	-- Stavy stlpcov
-	SIGNAL NEIGH_L  : MATRIX_T;
-	SIGNAL NEIGH_R  : MATRIX_T;
-	SIGNAL STAT_COL : MATRIX_T := (OTHERS => (OTHERS => '0'));
+	signal NEIGH_L  : MATRIX_T;
+	signal NEIGH_R  : MATRIX_T;
+	signal STAT_COL : MATRIX_T := (others => (others => '0'));
 
 	-- Vlajka pre koniec testu
-	SIGNAL DONE     : BOOLEAN  := FALSE;
-BEGIN
+	signal DONE : boolean := false;
+begin
 	-- Instanciacia troch susednych stlpcov
-	COLUMN_MAP : FOR i IN 0 TO 2 GENERATE
-		col_i : ENTITY work.column
-			PORT MAP(
+	COLUMN_MAP : for i in 0 to 2 generate
+		col_i : entity work.column
+			port map(
 				CLK         => CLK,
 				RESET       => RESET,
 				STATE       => STAT_COL(i),
@@ -52,7 +52,7 @@ BEGIN
 				DIRECTION   => DIRECTION,
 				EN          => EN
 			);
-	END GENERATE;
+	end generate;
 
 	-- Prepojenie susedov
 	NEIGH_L(0) <= STAT_COL(2);
@@ -63,148 +63,149 @@ BEGIN
 	NEIGH_R(2) <= STAT_COL(0);
 
 	-- Hodinovy proces
-	CLK_process : PROCESS BEGIN
-		IF DONE = TRUE THEN
-			WAIT;
-		END IF;
+	CLK_process : process
+	begin
+		if DONE = true then
+			wait;
+		end if;
 
 		CLK <= '0';
-		WAIT FOR CLK_period/2;
+		wait for CLK_period/2;
 		CLK <= '1';
-		WAIT FOR CLK_period/2;
-	END PROCESS;
+		wait for CLK_period/2;
+	end process;
 
 	-- Stimulus
-	stim_proc : PROCESS IS
-		VARIABLE passed   : BOOLEAN := FALSE;
-		VARIABLE pass_cnt : INTEGER := 0;
-	BEGIN
+	stim_proc : process is
+		variable passed   : boolean := false;
+		variable pass_cnt : integer := 0;
+	begin
 		RESET <= '1';
-		WAIT FOR 100 ns;
+		wait for 100 ns;
 		RESET <= '0';
-		WAIT FOR CLK_period;
+		wait for CLK_period;
 
-		REPORT "======================================" SEVERITY NOTE;
-		REPORT "*          column testbench          *" SEVERITY NOTE;
-		REPORT "======================================" SEVERITY NOTE;
-		REPORT " " SEVERITY NOTE;
+		report "======================================" severity note;
+		report "*          column testbench          *" severity note;
+		report "======================================" severity note;
+		report " " severity note;
 
 		-- Test pociatocneho stavu
-		passed := (STAT_COL(0) = X"AA") AND (STAT_COL(1) = X"CC")
-			AND (STAT_COL(2) = X"FF");
-		ASSERT passed = TRUE REPORT "FAIL! Pociatocny stav" SEVERITY ERROR;
-		ASSERT passed = FALSE REPORT "PASS! Pociatocny stav" SEVERITY NOTE;
+		passed := (STAT_COL(0) = X"AA") and (STAT_COL(1) = X"CC")
+				and (STAT_COL(2) = X"FF");
+		assert passed = true report "FAIL! Pociatocny stav" severity error;
+		assert passed = false report "PASS! Pociatocny stav" severity note;
 
 		-- Posun vlavo
 		DIRECTION <= DIR_LEFT;
 		EN        <= '1';
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"CC") AND (STAT_COL(1) = X"FF")
-			AND (STAT_COL(2) = X"AA");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vlavo #1" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed    := (STAT_COL(0) = X"CC") and (STAT_COL(1) = X"FF")
+				and (STAT_COL(2) = X"AA");
+		assert passed = true report "FAIL! Posun vlavo #1" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"FF") AND (STAT_COL(1) = X"AA")
-			AND (STAT_COL(2) = X"CC");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vlavo #2" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed := (STAT_COL(0) = X"FF") and (STAT_COL(1) = X"AA")
+				and (STAT_COL(2) = X"CC");
+		assert passed = true report "FAIL! Posun vlavo #2" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"AA") AND (STAT_COL(1) = X"CC")
-			AND (STAT_COL(2) = X"FF");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vlavo #3" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed := (STAT_COL(0) = X"AA") and (STAT_COL(1) = X"CC")
+				and (STAT_COL(2) = X"FF");
+		assert passed = true report "FAIL! Posun vlavo #3" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		ASSERT pass_cnt /= 3 REPORT "PASS! Posun vlavo" SEVERITY NOTE;
+		assert pass_cnt /= 3 report "PASS! Posun vlavo" severity note;
 		pass_cnt := 0;
 
 		-- Posun vpravo
 		DIRECTION <= DIR_RIGHT;
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"FF") AND (STAT_COL(1) = X"AA")
-			AND (STAT_COL(2) = X"CC");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vpravo #1" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed    := (STAT_COL(0) = X"FF") and (STAT_COL(1) = X"AA")
+				and (STAT_COL(2) = X"CC");
+		assert passed = true report "FAIL! Posun vpravo #1" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"CC") AND (STAT_COL(1) = X"FF")
-			AND (STAT_COL(2) = X"AA");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vpravo #2" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed := (STAT_COL(0) = X"CC") and (STAT_COL(1) = X"FF")
+				and (STAT_COL(2) = X"AA");
+		assert passed = true report "FAIL! Posun vpravo #2" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		WAIT FOR CLK_period;
-		passed := (STAT_COL(0) = X"AA") AND (STAT_COL(1) = X"CC")
-			AND (STAT_COL(2) = X"FF");
-		ASSERT passed = TRUE REPORT "FAIL! Posun vpravo #3" SEVERITY ERROR;
-		IF passed THEN
+		wait for CLK_period;
+		passed := (STAT_COL(0) = X"AA") and (STAT_COL(1) = X"CC")
+				and (STAT_COL(2) = X"FF");
+		assert passed = true report "FAIL! Posun vpravo #3" severity error;
+		if passed then
 			pass_cnt := pass_cnt + 1;
-		END IF;
+		end if;
 
-		ASSERT pass_cnt /= 3 REPORT "PASS! Posun vpravo" SEVERITY NOTE;
+		assert pass_cnt /= 3 report "PASS! Posun vpravo" severity note;
 		pass_cnt := 0;
 
 		-- Rolovanie nahor
 		DIRECTION <= DIR_TOP;
 
-		FOR i IN 1 TO 8 LOOP
-			WAIT FOR CLK_period;
+		for i in 1 to 8 loop
+			wait for CLK_period;
 
-			passed := (UNSIGNED(STAT_COL(0)) = X"AA" SLL i)
-				AND (UNSIGNED(STAT_COL(1)) = X"CC" SLL i)
-				AND (UNSIGNED(STAT_COL(2)) = X"FF" SLL i);
-			ASSERT passed = TRUE REPORT "FAIL! Rolovanie nahor #" & INTEGER'IMAGE(i) SEVERITY ERROR;
-			IF passed THEN
+			passed := (unsigned(STAT_COL(0)) = X"AA" srl i)
+					and (unsigned(STAT_COL(1)) = X"CC" srl i)
+					and (unsigned(STAT_COL(2)) = X"FF" srl i);
+			assert passed = true report "FAIL! Rolovanie nahor #" & integer'image(i) severity error;
+			if passed then
 				pass_cnt := pass_cnt + 1;
-			END IF;
-		END LOOP;
+			end if;
+		end loop;
 
-		ASSERT pass_cnt /= 8 REPORT "PASS! Rolovanie nahor" SEVERITY NOTE;
+		assert pass_cnt /= 8 report "PASS! Rolovanie nahor" severity note;
 
 		-- Reset
-		RESET <= '1';
-		WAIT FOR CLK_period;
-		RESET <= '0';
-		passed := (STAT_COL(0) = X"AA") AND (STAT_COL(1) = X"CC")
-			AND (STAT_COL(2) = X"FF");
-		ASSERT passed = TRUE REPORT "FAIL! Reset" SEVERITY ERROR;
-		ASSERT passed = FALSE REPORT "PASS! Reset" SEVERITY NOTE;
+		RESET  <= '1';
+		wait for CLK_period;
+		RESET  <= '0';
+		passed := (STAT_COL(0) = X"AA") and (STAT_COL(1) = X"CC")
+				and (STAT_COL(2) = X"FF");
+		assert passed = true report "FAIL! Reset" severity error;
+		assert passed = false report "PASS! Reset" severity note;
 
 		-- Ripple (vlastny efekt)
 		DIRECTION <= DIR_TOP_BOTTOM;
 
-		WAIT FOR CLK_period;
-		ASSERT (STAT_COL(0) = X"45") AND (STAT_COL(1) = X"86")
-		AND (STAT_COL(2) = X"EF") REPORT "FAIL! Ripple #1" SEVERITY ERROR;
+		wait for CLK_period;
+		assert (STAT_COL(0) = X"45") and (STAT_COL(1) = X"86")
+			and (STAT_COL(2) = X"EF") report "FAIL! Ripple #1" severity error;
 
-		WAIT FOR CLK_period;
-		ASSERT (STAT_COL(0) = X"82") AND (STAT_COL(1) = X"03")
-		AND (STAT_COL(2) = X"C7") REPORT "FAIL! Ripple #2" SEVERITY ERROR;
+		wait for CLK_period;
+		assert (STAT_COL(0) = X"82") and (STAT_COL(1) = X"03")
+			and (STAT_COL(2) = X"C7") report "FAIL! Ripple #2" severity error;
 
-		WAIT FOR CLK_period;
-		ASSERT (STAT_COL(0) = X"01") AND (STAT_COL(1) = X"01")
-		AND (STAT_COL(2) = X"83") REPORT "FAIL! Ripple #3" SEVERITY ERROR;
+		wait for CLK_period;
+		assert (STAT_COL(0) = X"01") and (STAT_COL(1) = X"01")
+			and (STAT_COL(2) = X"83") report "FAIL! Ripple #3" severity error;
 
-		WAIT FOR CLK_period;
-		ASSERT (STAT_COL(0) = X"00") AND (STAT_COL(1) = X"00")
-		AND (STAT_COL(2) = X"01") REPORT "FAIL! Ripple #4" SEVERITY ERROR;
+		wait for CLK_period;
+		assert (STAT_COL(0) = X"00") and (STAT_COL(1) = X"00")
+			and (STAT_COL(2) = X"01") report "FAIL! Ripple #4" severity error;
 
-		WAIT FOR CLK_period;
-		ASSERT (STAT_COL(0) = X"00") AND (STAT_COL(1) = X"00")
-		AND (STAT_COL(2) = X"00") REPORT "FAIL! Ripple #5" SEVERITY ERROR;
+		wait for CLK_period;
+		assert (STAT_COL(0) = X"00") and (STAT_COL(1) = X"00")
+			and (STAT_COL(2) = X"00") report "FAIL! Ripple #5" severity error;
 
 		EN   <= '0';
-		DONE <= TRUE;
-		WAIT;
-	END PROCESS;
-END;
+		DONE <= true;
+		wait;
+	end process;
+end;
