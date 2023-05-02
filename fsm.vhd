08eff64004a104bfa516518d30732b0472d01e93
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-05-02 21:17:39 +0200

    fix: make `fsm` synthesisable

--- fsm.vhd
+++ fsm.vhd
@@ -26,7 +26,7 @@ ARCHITECTURE behavioral OF fsm IS
 	SIGNAL CNT_ROT  : INTEGER := 0;              -- Citac otociek
 	SIGNAL CNT_STEP : INTEGER := 0;              -- Citac krokov
 BEGIN
-	state_logic : PROCESS (CLK) IS
+	state_logic : PROCESS (CLK, RST) IS
 	BEGIN
 		IF RST = '1' THEN
 			pstate   <= RIGHT_ROTATION;
@@ -35,69 +35,75 @@ BEGIN
 			COL_EN   <= '0';
 			COL_RST  <= '1';
 		ELSIF rising_edge(CLK) AND EN = '1' THEN
-			CNT_STEP <= CNT_STEP + 1;
-			COL_EN   <= '1';
+			IF EN = '1' THEN
+				CNT_STEP <= CNT_STEP + 1;
 
-			CASE pstate IS
-				WHEN RIGHT_ROTATION =>
-					DIRECTION <= DIR_RIGHT;
+				CASE pstate IS
+					WHEN RIGHT_ROTATION =>
+						DIRECTION <= DIR_RIGHT;
 
-					IF (CNT_STEP = 16 - 1) THEN
-						CNT_ROT  <= CNT_ROT + 1;
-						CNT_STEP <= 0;
-					END IF;
+						IF (CNT_STEP = 16 - 1) THEN
+							CNT_ROT  <= CNT_ROT + 1;
+							CNT_STEP <= 0;
+						END IF;
 
-					IF (CNT_ROT = 3) THEN
-						CNT_ROT  <= 0;
-						CNT_STEP <= 0;
-						COL_EN   <= '0';
-						COL_RST  <= '1';
-						pstate   <= LEFT_ROTATION;
-					ELSE
-						COL_RST <= '0';
-					END IF;
+						IF (CNT_ROT = 3) THEN
+							CNT_ROT  <= 0;
+							CNT_STEP <= 0;
+							COL_EN   <= '0';
+							COL_RST  <= '1';
+							pstate   <= LEFT_ROTATION;
+						ELSE
+							COL_EN  <= '1';
+							COL_RST <= '0';
+						END IF;
 
-				WHEN LEFT_ROTATION =>
-					DIRECTION <= DIR_LEFT;
+					WHEN LEFT_ROTATION =>
+						DIRECTION <= DIR_LEFT;
 
-					IF (CNT_STEP = 16 - 1) THEN
-						CNT_ROT  <= CNT_ROT + 1;
-						CNT_STEP <= 0;
-					END IF;
+						IF (CNT_STEP = 16 - 1) THEN
+							CNT_ROT  <= CNT_ROT + 1;
+							CNT_STEP <= 0;
+						END IF;
 
-					IF (CNT_ROT = 3) THEN
-						CNT_ROT  <= 0;
-						CNT_STEP <= 0;
-						COL_EN   <= '0';
-						COL_RST  <= '1';
-						pstate   <= ROLL_UP;
-					ELSE
-						COL_RST <= '0';
-					END IF;
+						IF (CNT_ROT = 3) THEN
+							CNT_ROT  <= 0;
+							CNT_STEP <= 0;
+							COL_EN   <= '0';
+							COL_RST  <= '1';
+							pstate   <= ROLL_UP;
+						ELSE
+							COL_RST <= '0';
+							COL_EN  <= '1';
+						END IF;
 
-				WHEN ROLL_UP =>
-					DIRECTION <= DIR_TOP;
+					WHEN ROLL_UP =>
+						DIRECTION <= DIR_TOP;
 
-					IF (CNT_STEP = 8) THEN
-						CNT_ROT  <= 0;
-						CNT_STEP <= 0;
-						COL_EN   <= '0';
-						COL_RST  <= '1';
-						pstate   <= RIPPLE;
-					ELSE
-						COL_RST <= '0';
-					END IF;
+						IF (CNT_STEP = 8) THEN
+							CNT_ROT  <= 0;
+							CNT_STEP <= 0;
+							COL_EN   <= '0';
+							COL_RST  <= '1';
+							pstate   <= RIPPLE;
+						ELSE
+							COL_RST <= '0';
+							COL_EN  <= '1';
+						END IF;
 
-				WHEN RIPPLE =>
-					DIRECTION <= DIR_TOP_BOTTOM;
-					COL_RST   <= '0';
+					WHEN RIPPLE =>
+						DIRECTION <= DIR_TOP_BOTTOM;
+						COL_RST   <= '0';
 
-					IF (CNT_STEP = (8 / 2) + 1) THEN
-						COL_EN <= '0';
-					END IF;
-			END CASE;
-		ELSE
-			COL_EN <= '0';
+						IF (CNT_STEP = (8 / 2) + 1) THEN
+							COL_EN <= '0';
+						ELSE
+							COL_EN <= '1';
+						END IF;
+				END CASE;
+			ELSE
+				COL_EN <= '0';
+			END IF;
 		END IF;
 	END PROCESS;
 END ARCHITECTURE behavioral;
