34ddc247abcd56cd26977cd8dbb20a0b8b259215
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-05-02 21:18:20 +0200

    fix: use vector instead of an array

--- rom.vhd
+++ rom.vhd
@@ -7,17 +7,23 @@ USE work.effects_pack.ALL;
 
 ENTITY rom IS
 	PORT (
-		ADDR : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-		DATA : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+		ADDR : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+		DATA : OUT STD_LOGIC_VECTOR(127 DOWNTO 0)
 	);
 END ENTITY rom;
 
 ARCHITECTURE behavioral OF rom IS
-	CONSTANT rom_arr : MATRIX_T := (
-		"01011100", "00111010", "00011100", "01111011",
-		"11011110", "10111010", "10011100", "11111011",
-		"11101110", "11011101", "10011110", "11110111",
-		"01111011", "10111011", "01011110", "01110111"
+	TYPE ROM_T IS ARRAY (0 TO 1) OF STD_LOGIC_VECTOR(0 TO 127);
+	CONSTANT rom_arr : ROM_T := (
+		X"FF" & X"EE" & X"DD" & X"CC" &
+		X"BB" & X"AA" & X"99" & X"88" &
+		X"77" & X"66" & X"55" & X"44" &
+		X"33" & X"22" & X"11" & X"00",
+
+		X"00" & X"00" & X"00" & X"00" &
+		X"00" & X"00" & X"00" & X"00" &
+		X"00" & X"00" & X"00" & X"00" &
+		X"00" & X"00" & X"00" & X"00"
 	);
 BEGIN
 	DATA <= rom_arr(to_integer(unsigned(ADDR)));
